8cf661db4aa8cbc17e40d18bbb67fba19a2e6c57
PermanentPon <andrey.ponikar@gmail.com>
2017-05-28 19:49:34 +0300

    Make BIC and DIC

--- my_model_selectors.py
+++ my_model_selectors.py
@@ -75,9 +75,25 @@ class SelectorBIC(ModelSelector):
         :return: GaussianHMM object
         """
         warnings.filterwarnings("ignore", category=DeprecationWarning)
+        #try:
+        bic_previous = 1000000
+        best_model = None
+        for num_states in range(self.min_n_components, self.max_n_components+1):
+            hmm_model = GaussianHMM(n_components=num_states, covariance_type="diag", n_iter=1000,
+                                    random_state=self.random_state, verbose=False).fit(self.X, self.lengths)
+            logL = hmm_model.score(self.X, self.lengths)
+            bic = -2 * logL + num_states * len(self.lengths)
+            if bic < bic_previous:
+                bic_previous = bic
+                best_model = hmm_model
+            #if self.verbose:
+            #    print("model created for {} with {} states".format(self.this_word, num_states))
+        return best_model
+        #except:
+        #    if self.verbose:
+        #        print("failure on {} with {} states".format(self.this_word, num_states))
+        #   return None
 
-        # TODO implement model selection based on BIC scores
-        raise NotImplementedError
 
 
 class SelectorDIC(ModelSelector):
@@ -91,9 +107,26 @@ class SelectorDIC(ModelSelector):
 
     def select(self):
         warnings.filterwarnings("ignore", category=DeprecationWarning)
-
-        # TODO implement model selection based on DIC scores
-        raise NotImplementedError
+        #logL_list = list()
+        dic_previous = -100000.0
+        for num_states in range(self.min_n_components, self.max_n_components):
+            hmm_model = GaussianHMM(n_components=num_states, covariance_type="diag", n_iter=1000,
+                                    random_state=self.random_state, verbose=False).fit(self.X, self.lengths)
+            print(hmm_model)
+            logL = hmm_model.score(self.X, self.lengths)
+            logL_word_sum = 0.0
+            for word in self.hwords:
+                if word != self.this_word:
+                    X_1, lengths_1 = self.hwords[word]
+                    logL_word = hmm_model.score(X_1, lengths_1)
+                    logL_word_sum += logL_word
+            dic = logL - 1/(len(self.hwords) - 1) * logL_word_sum
+            print(dic)
+            #logL_list.append((hmm_model, logL))
+            if dic > dic_previous:
+                dic_previous = dic
+                best_model = hmm_model
+        return best_model
 
 
 class SelectorCV(ModelSelector):
